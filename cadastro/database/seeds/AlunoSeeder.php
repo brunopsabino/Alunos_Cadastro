6372d55f7197ab3dfbda18d1eea053a02792515e
brunopsabino <bruno.p.sabino@gmail.com>
2018-06-27 00:12:22 -0300

    Seeds

--- cadastro/database/seeds/AlunoSeeder.php
+++ cadastro/database/seeds/AlunoSeeder.php
@@ -14,6 +14,26 @@ class AlunoSeeder extends Seeder
         VALUES (?,?,?,?,?)',
         array('Bruno Sabino', 4, 23782, 4, 1));
 
+        DB::insert('INSERT INTO aluno
+        (nome_aluno, curso, numero_matricula, semestre, status_aluno)
+        VALUES (?,?,?,?,?)',
+        array('Jose Pereira', 2, 1234, 2, 2));
+
+        DB::insert('INSERT INTO aluno
+        (nome_aluno, curso, numero_matricula, semestre, status_aluno)
+        VALUES (?,?,?,?,?)',
+        array('Maria Gadelha', 1, 38948, 7, 3));
+
+        DB::insert('INSERT INTO aluno
+        (nome_aluno, curso, numero_matricula, semestre, status_aluno)
+        VALUES (?,?,?,?,?)',
+        array('Gabriel Pinheiro', 3, 45275, 6, 1));
+
+        DB::insert('INSERT INTO aluno
+        (nome_aluno, curso, numero_matricula, semestre, status_aluno)
+        VALUES (?,?,?,?,?)',
+        array('Fernanda Silva', 1, 13452, 7, 1));
+
         
     }
 }
